6e7389281d64a8fe0df232868fa90c8408177a2e
yewonkim <kyw9791@naver.com>
2024-10-16 17:51:59 +0000

    train

--- quantization.py
+++ quantization.py
@@ -2,14 +2,12 @@ import sys
 sys.path.append('/home/ywkim/anaconda3/lib/python3.10/site-packages')
 
 import torch
-import torch_pruning as tp
 
 def quantize_qint8(net):
     example_inputs = torch.randn(1, 1, 32, 32)
     model_dynamic_quantized = torch.quantization.quantize_dynamic(
         net.to('cpu'), qconfig_spec={torch.nn.Linear}, dtype=torch.qint8
     ).to('cpu')
-    #quantize_macs, quantize_nparams = tp.utils.count_ops_and_params(model_dynamic_quantized, example_inputs.to('cpu'))
 
 
 
